89de390c185c5236c1a24d78a14625a86466aee8
ROHIT-18-05 <rohit364710@gmail.com>
2026-01-02 09:18:33 +0530

    more Practice

--- index.js
+++ index.js
@@ -20,7 +20,17 @@ const myfun = (a,b) => a + b;{
     });
 
 
-    // 1. Create a 
+    // 1. Create a endpoint Student which will print Student Details
+    app.get('/student', (req, res) => {
+        res.send("Student Details: Name: John Doe, Age: 20, Course: Computer Science");
+    })
     // 2. Print RKU
+    app.get('/university', (req, res) => {
+        res.send("RK University");
+    })
     // 3. Create a endpoint which will accept name and last name and print full name.
-    // 
+    app.get('/fullname', (req, res) => {
+        const firstName = "Rohit";
+        const lastName = "Dabhi";
+        res.send(`Full Name: ${firstName} ${lastName}`);
+    })
